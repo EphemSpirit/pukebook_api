1130942fa0a143a17b23f13b175160e34209d587
Drew Hund <languageinvestigator@gmail.com>
2023-05-27 15:26:18 -0400

    users model set up. no controlle rneeded at this point

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+    def configure_premitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :first_name, :last_name])
+      devise_parameter_sanitizer.permit(:sign_in, keys: [:username])
+    end
 end
