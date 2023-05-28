3a586aad0cb2e741556c74dcb0c4a3d4279cbe44
Drew Hund <languageinvestigator@gmail.com>
2023-05-28 13:45:34 -0400

    added model spec for mini blarghs

--- spec/models/mini_blargh_spec.rb
+++ spec/models/mini_blargh_spec.rb
@@ -3,5 +3,25 @@
 require 'rails_helper'
 
 RSpec.describe MiniBlargh, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'when creating a mini blargh' do
+    let(:blargh) { create(:blargh) }
+    let(:mini_blargh) { build(:mini_blargh, blargh_id: blargh.id, author_id: blargh.author.id) }
+    describe '#save' do
+      it 'saves the record' do
+        expect { mini_blargh.save }.to change(MiniBlargh, :count).by(1)
+      end
+    end
+  end
+
+  context 'when no body' do
+    let(:blargh) { create(:blargh) }
+    let(:mini_blargh) { build(:mini_blargh, blargh_id: blargh.id, author_id: blargh.author.id) }
+    describe '#save' do
+      it "doesn't persist" do
+        mini_blargh.body = ''
+        expect { mini_blargh.save }.to change(MiniBlargh, :count).by(0)
+        expect(mini_blargh.errors.full_messages).to eq(["Body can't be blank"])
+      end
+    end
+  end
 end
